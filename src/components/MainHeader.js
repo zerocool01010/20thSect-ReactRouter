6a4e328073133a3eabcc053072910894746c46f2
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-12 17:27:06 -0300

    Home link agregado para redirigirse al home page

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -7,6 +7,9 @@ const MainHeader = () => {
     <header className={classes.header}>
       <nav>
         <ul>
+          <li className="home">
+            <Link to='/'>Home</Link>
+          </li>
           <li>
             <Link to="/welcome">Welcome</Link>
           </li>
