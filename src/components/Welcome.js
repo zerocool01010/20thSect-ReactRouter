7e493ee4af2d09308980cd047c5ba4422f7823ed
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-12 16:56:02 -0300

    Agrego un state para decidir logicamente lo que hace un link

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,21 @@
-import react from "react";
+import React, { useState } from "react";
+import { Route, Link } from "react-router-dom";
 
 const Welcome = () => {
-    return <h2>Welcome to all men!</h2>
-}
+    const [linkVisited, setLinkVisited] = useState(false)
 
-export default Welcome
+    const linkHandler = () => {
+        setLinkVisited(!linkVisited)
+    }
+
+  return (
+    <section>
+      <Link onClick={linkHandler} to={!linkVisited ? "/welcome/new-user" : '/welcome'}>Welcome to all men!</Link>
+      <Route path="/welcome/new-user">
+        <p>Welcome again</p>
+      </Route>
+    </section>
+  );
+};
+
+export default Welcome;
