269c323361c76d5d82660bdf28aa39f5d24f727e
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-12 13:13:49 -0300

    Instale el react router v5 porque la v6 esta en beta creo y trae complicaciones innecesarias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Routes>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Products = () => {
+    return <h2>Hello stranger! What are you buying? What are you selling?</h2>
+}
+
+export default Products
