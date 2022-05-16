d076e36f5e566a6415f3238833a44855cdbfacac
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-16 16:54:48 -0300

    Uso parametros para definir un nested route component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,16 @@
+import React, { useParams, Route } from "react-router-dom";
+import Comments from '../components/comments/Comments'
 const QuoteDetail = () => {
-  return <div>Quote detail is here</div>;
+  const urlParam = useParams();
+  console.log(urlParam.quoteId);
+  return (
+    <>
+      <div>Quote detail is here: </div>
+      <Route path={`/quotes/${urlParam.quoteId}/comments`}>
+        <Comments/>
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetail;
