97e09e2c35b53abaf52fa94569ab5d1c7bab8f49
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-16 17:53:10 -0300

    Finalizado video 281 del curso donde se nos deja una actividad antes de pasar al 282

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList'
+const dummy_quotes = [
+    { id: 'q1', author: 'the guy from Scream', text: 'it was FUN!'},
+    { id: 'q2', author: 'Vincent Law', text: 'ware wa Ergo Proxy, the angel of death'},
+    { id: 'q3', author: 'Yagami Light', text: 'Boku ga Kira, so shite, shinse ka no kami-da'}
+]
+
 const AllQuotes = () => {
-  return <div>All Quotes are here</div>;
+  return <QuoteList quotes={dummy_quotes}/>;
 };
 
 export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,11 @@
+import QuoteForm from '../components/quotes/QuoteForm'
+
 const NewQuote = () => {
-  return <div>New quote is here</div>;
+    const onAddQuoteHandler = (quoteData) => {
+        console.log(quoteData)
+    }
+
+  return <QuoteForm onAddQuote={onAddQuoteHandler}/>
 };
 
 export default NewQuote;
